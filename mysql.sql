fd33644c93fa711790219f3edd2e7f424321f80a
toryzen <toryzen@Hotmail.com>
2014-05-21 11:51:52 +0800

    mysql

--- mysql.sql
+++ mysql.sql
@@ -26,6 +26,10 @@ CREATE TABLE `rbac_auth` (
   UNIQUE KEY `nid_rid` (`node_id`,`role_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='角色与节点对应表';
 
+/*Data for the table `rbac_auth` */
+
+insert into `rbac_auth` (`node_id`,`role_id`) values (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,1),(15,1),(16,1),(17,1),(18,1);
+
 /*Table structure for table `rbac_menu` */
 
 DROP TABLE IF EXISTS `rbac_menu`;
@@ -39,6 +43,10 @@ CREATE TABLE `rbac_menu` (
   `status` int(11) DEFAULT '1' COMMENT '状态(1:正常,0:停用)',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='菜单表';
+
+/*Data for the table `rbac_menu` */
+
+insert into `rbac_menu` (`id`,`title`,`node_id`,`p_id`,`sort`,`status`) values (1,'后台管理',NULL,NULL,9,1),(2,'节点管理',5,1,2,1),(3,'导航管理',1,1,1,1),(4,'人员管理',14,1,4,1),(5,'角色管理',9,1,3,1),(6,'一级菜单',0,NULL,0,1),(7,'二级节点',18,6,1,1),(8,'一级菜单2',NULL,NULL,2,1),(9,'二级菜单',NULL,8,1,1),(10,'三级节点',18,9,1,1);
 
 /*Table structure for table `rbac_node` */
 
@@ -55,6 +63,10 @@ CREATE TABLE `rbac_node` (
   UNIQUE KEY `d_c_f` (`dirc`,`cont`,`func`)
 ) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='节点表';
 
+/*Data for the table `rbac_node` */
+
+insert into `rbac_node` (`id`,`dirc`,`cont`,`func`,`memo`,`status`) values (1,'manage','menu','index','导航管理',1),(2,'manage','menu','edit','导航修改',1),(3,'manage','menu','delete','导航删除',1),(4,'manage','menu','add','导航新增',1),(5,'manage','node','index','节点管理',1),(6,'manage','node','add','节点新增',1),(7,'manage','node','delete','节点删除',1),(8,'manage','node','edit','节点修改',1),(9,'manage','role','index','角色管理',1),(10,'manage','role','action','角色赋权',1),(11,'manage','role','delete','角色删除',1),(12,'manage','role','edit','角色修改',1),(13,'manage','role','add','角色新增',1),(14,'manage','member','index','人员管理',1),(15,'manage','member','edit','人员修改',1),(16,'manage','member','delete','人员删除',1),(17,'manage','member','add','人员新增',1),(18,'product','index','index','测试用节点',1);
+
 /*Table structure for table `rbac_role` */
 
 DROP TABLE IF EXISTS `rbac_role`;
@@ -66,6 +78,10 @@ CREATE TABLE `rbac_role` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `rolename` (`rolename`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+/*Data for the table `rbac_role` */
+
+insert into `rbac_role` (`id`,`rolename`,`status`) values (1,'管理员',1);
 
 /*Table structure for table `rbac_user` */
 
@@ -83,6 +99,10 @@ CREATE TABLE `rbac_user` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Data for the table `rbac_user` */
+
+insert into `rbac_user` (`id`,`username`,`password`,`nickname`,`email`,`role_id`,`status`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3','toryzen','admin@admin.com',1,1);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
